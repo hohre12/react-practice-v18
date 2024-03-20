d3f0f3fc70534fe88fb9b05e026655a1a54043a7
hohre12 <60336857+hohre12@users.noreply.github.com>
2024-03-20 22:04:52 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,14 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+            retry: 0
+        }
+    }
+})
 // 1. useQuiz hooks 사용하기 ( 데이터와 UI 분리 ) - Headless 기반의 추상화
 // 2. SSR로 구현해보기 ( 나중에 nextJS로도 구현 )
 
