35a6797940a4011ec288641ec115f6a538bcf989
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-03-25 13:06:02 +0900

    Update quiz.ts

--- src/type/quiz.ts
+++ src/type/quiz.ts
@@ -1,22 +1,3 @@
-// export const quizStatusCode = {
-// 	start: 'start',
-// 	ing: 'ing',
-// 	complete: 'complete'
-// }
-
-// export type TquizStatus = typeof quizStatusCode[keyof typeof quizStatusCode]
-
-
-// export type TQuiz = {
-// 	category: string,
-// 	correct_answer: string,
-// 	difficulty: string,
-// 	incorrect_answers: []
-// 	question: string,
-// 	type: string,
-// 	options?: [string]
-// }
-
 export type TQuizHeader = {
     category: string,
     difficulty: string,
