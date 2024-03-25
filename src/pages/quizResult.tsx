6547321b465c054dfe9203725c5b56fddc9f2839
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-03-25 16:29:33 +0900

    ADD] code 리팩토링

--- src/pages/quizResult.tsx
+++ src/pages/quizResult.tsx
@@ -1,6 +1,7 @@
 import { useRecoilValue } from "recoil"
 import { quizListState, selectAnswerListState, timeState } from "../recoil/atoms/quiz"
 import { Link } from "react-router-dom"
+import { TQuiz } from "../type/quiz"
 
 
 
@@ -9,18 +10,14 @@ const QuizResult = () => {
     const selectAnswerList = useRecoilValue(selectAnswerListState)
     const time = useRecoilValue(timeState)
     
-    let rightAnswer: number[] = []
-    let wrongAnswer: number[] = []
+    // 굳이 useMemo를 안써도 될거같음.
+    const rightAnswer: TQuiz[] = quizList.filter((it, idx) => it.correct_answer === selectAnswerList[idx])
 
-    for(let i = 0; i < quizList.length; i++) {
-        if(selectAnswerList[i] === quizList[i].correct_answer) rightAnswer.push(i)
-        else wrongAnswer.push(i)
-    }
     return <div>
         <h1>결과</h1>
         <h2>경과시간 : {time} 초</h2>
         <h2>정답갯수 : {rightAnswer?.length} 개</h2>
-        <h2>오답갯수 : {wrongAnswer?.length} 개</h2>
+        <h2>오답갯수 : {quizList.length - rightAnswer.length} 개</h2>
         <Link to='/note'>오답노트 가기</Link>
     </div>
 }
