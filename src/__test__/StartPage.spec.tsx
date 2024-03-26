11dfee455a7a1d61cfa5754710ce04939f8f88ab
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-03-26 16:24:29 +0900

    ADD] jest 초기 설정중

---
+++ src/__test__/StartPage.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import StartPage from '../pages';
+
+const setup = () => {
+  return render(<StartPage />);
+};
+
+describe('퀴즈시작 메인 페이지 컴포넌트 테스트', () => {
+  test('퀴즈시작 버튼 클릭을 했을때, 퀴즈 페이지로 넘어간다.', async () => {
+    setup();
+    await userEvent.click(screen.getByText('퀴즈풀기'));
+    // await waitFor(() => {
+    //     expect()
+    // })
+  });
+});
