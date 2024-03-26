670773612e5ca481d8bd744f3504e7484708cc35
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-03-26 16:49:57 +0900

    ADD] StartPage - Jest 셋팅

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+  },
+  modulePaths: ['<rootDir>/src/'],
+  //   moduleNameMapper: {
+  //     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  //   },
+  testEnvironment: 'jsdom',
+};
--- src/__test__/StartPage.spec.tsx
+++ src/__test__/StartPage.spec.tsx
@@ -2,8 +2,16 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import StartPage from '../pages';
 
+const TestComponent = () => {
+  return (
+    <div>
+      <button onClick={() => console.log('테스트')}>퀴즈풀기</button>
+    </div>
+  );
+};
+
 const setup = () => {
-  return render(<StartPage />);
+  return render(<TestComponent />);
 };
 
 describe('퀴즈시작 메인 페이지 컴포넌트 테스트', () => {
