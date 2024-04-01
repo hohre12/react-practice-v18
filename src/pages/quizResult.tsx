f6872ebef5ffb191ed8230376e4e7a9e2343e717
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2024-04-01 16:21:36 +0900

    ADD] chart 기능 넣기

--- src/pages/quizResult.tsx
+++ src/pages/quizResult.tsx
@@ -3,6 +3,7 @@ import { quizListState, selectAnswerListState, timeState } from '../recoil/atoms
 import { Link } from 'react-router-dom';
 import { TQuiz } from '../type/quiz';
 import { useInternalRouter } from '../hooks/useInternalRouter';
+import { ResponsivePie } from '@nivo/pie';
 
 const QuizResult = () => {
   const router = useInternalRouter();
@@ -12,7 +13,18 @@ const QuizResult = () => {
 
   // 굳이 useMemo를 안써도 될거같음.
   const rightAnswer: TQuiz[] = quizList.filter((it, idx) => it.correct_answer === selectAnswerList[idx]);
-
+  const pieData = [
+    {
+      id: 'right',
+      label: '정답',
+      value: rightAnswer.length,
+    },
+    {
+      id: 'wrong',
+      label: '오답',
+      value: quizList.length - rightAnswer.length,
+    },
+  ];
   return (
     <div>
       <h1>결과</h1>
@@ -20,6 +32,20 @@ const QuizResult = () => {
       <h2>정답갯수 : {rightAnswer?.length} 개</h2>
       <h2>오답갯수 : {quizList.length - rightAnswer.length} 개</h2>
       <button onClick={() => router.push('/note')}>오답노트 가기</button>
+      <div style={{ width: '500px', height: '800px' }}>
+        <ResponsivePie
+          data={pieData}
+          colors={['olive', 'orange']}
+          theme={{
+            labels: {
+              text: {
+                fontSize: 14,
+                fill: '#000000',
+              },
+            },
+          }}
+        ></ResponsivePie>
+      </div>
     </div>
   );
 };
